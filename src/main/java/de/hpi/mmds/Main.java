5edd72c859b8f626639a819348e98e6b965bbf77
Parnswir <maximiliangrundke@googlemail.com>
2016-06-23 20:47:18 +0200

    Switch to one file for to allow for use on the cluster

--- src/main/java/de/hpi/mmds/Main.java
+++ src/main/java/de/hpi/mmds/Main.java
@@ -45,187 +45,183 @@ public class Main {
 
         if (args.length == 1) {
             reviewPath = args[0];
+        } else {
+            File folder = new File(reviewPath);
+            File[] reviewFiles = folder.listFiles((dir, name) -> name.endsWith(".json"));
+            reviewPath = reviewFiles[0].getAbsolutePath();
         }
 
-        File folder = new File(reviewPath);
-        File[] reviewFiles = folder.listFiles((dir, name) -> name.endsWith(".json"));
+        JavaRDD<String> fileRDD = context.textFile(reviewPath);
 
-        for (File file : reviewFiles) {
+        JavaRDD<ReviewRecord> recordsRDD = fileRDD.map(JsonReader::readReviewJson);
 
-            JavaRDD<String> fileRDD = context.textFile(file.getAbsolutePath());
+        JavaPairRDD<List<TaggedWord>, Float> tagRDD = recordsRDD.mapToPair(
+                (ReviewRecord x) -> new Tuple2(Utility.posTag(x.getReviewText()), x.getOverall()));
 
-            JavaRDD<ReviewRecord> recordsRDD = fileRDD.map(JsonReader::readReviewJson);
+        tagRDD.cache();
 
-            JavaPairRDD<List<TaggedWord>, Float> tagRDD = recordsRDD.mapToPair(
-                    (ReviewRecord x) -> new Tuple2(Utility.posTag(x.getReviewText()), x.getOverall()));
+        JavaRDD<List<String>> textRdd = tagRDD.map(
+                a -> a._1.stream().map(Word::word).collect(Collectors.toList())
+        );
 
-            tagRDD.cache();
-
-            JavaRDD<List<String>> textRdd = tagRDD.map(
-                    a -> a._1.stream().map(Word::word).collect(Collectors.toList())
-            );
-
-            // Learn a mapping from words to Vectors.
-            Word2Vec word2Vec = new Word2Vec()
-                    .setVectorSize(150)
-                    .setMinCount(0);
-            Word2VecModel model = word2Vec.fit(textRdd);
+        // Learn a mapping from words to Vectors.
+        Word2Vec word2Vec = new Word2Vec()
+                .setVectorSize(150)
+                .setMinCount(0);
+        Word2VecModel model = word2Vec.fit(textRdd);
 
 
-            Template template = new NounNounTemplate();
-            JavaRDD<List<Tuple2<List<TaggedWord>, Integer>>> rddValuesRDD = tagRDD.map(
-                    taggedWords -> BigramThesis.findKGramsEx(3, taggedWords._1(), template)
-            );
+        Template template = new NounNounTemplate();
+        JavaRDD<List<Tuple2<List<TaggedWord>, Integer>>> rddValuesRDD = tagRDD.map(
+                taggedWords -> BigramThesis.findKGramsEx(3, taggedWords._1(), template)
+        );
 
-            JavaPairRDD<List<TaggedWord>, Integer> semiFinalRDD = rddValuesRDD.flatMapToPair(a -> a).reduceByKey(
-                    (a, b) -> a + b);
+        JavaPairRDD<List<TaggedWord>, Integer> semiFinalRDD = rddValuesRDD.flatMapToPair(a -> a).reduceByKey(
+                (a, b) -> a + b);
 
-            JavaPairRDD<Integer, List<TaggedWord>> swappedFinalRDD = semiFinalRDD.mapToPair(Tuple2::swap).sortByKey(
-                    false);
+        JavaPairRDD<Integer, List<TaggedWord>> swappedFinalRDD = semiFinalRDD.mapToPair(Tuple2::swap).sortByKey(
+                false);
 
-            JavaPairRDD<List<TaggedWord>, Integer> finalRDD = swappedFinalRDD.mapToPair(Tuple2::swap);
+        JavaPairRDD<List<TaggedWord>, Integer> finalRDD = swappedFinalRDD.mapToPair(Tuple2::swap);
 
-            JavaPairRDD<List<VectorWithWords>, Integer> vectorRDD = finalRDD.mapToPair(a -> {
-                List<VectorWithWords> vectors = a._1.stream().map(
-                        taggedWord -> new VectorWithWords(model.transform(taggedWord.word()),
-                                new ArrayList<>(Arrays.asList(taggedWord))
-                        )).collect(Collectors.toList());
-                return new Tuple2<>(vectors, a._2);
-            });
+        JavaPairRDD<List<VectorWithWords>, Integer> vectorRDD = finalRDD.mapToPair(a -> {
+            List<VectorWithWords> vectors = a._1.stream().map(
+                    taggedWord -> new VectorWithWords(model.transform(taggedWord.word()),
+                            new ArrayList<>(Arrays.asList(taggedWord))
+                    )).collect(Collectors.toList());
+            return new Tuple2<>(vectors, a._2);
+        });
 
-            List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> clusters = vectorRDD.aggregate(
-                    new LinkedList<>(),
-                    (List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> acc, Tuple2<List<VectorWithWords>, Integer> value) -> {
+        List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> clusters = vectorRDD.aggregate(
+                new LinkedList<>(),
+                (List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> acc, Tuple2<List<VectorWithWords>, Integer> value) -> {
+                    Boolean foundOne = false;
+                    List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> new_acc = new LinkedList<>(acc);
+                    for (int i = 0; i<acc.size(); i++){
+                        Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>> l = acc.get(i);
+                        if(compare(value._1(), l._1())){
+                            new_acc.remove(i);
+                            Set<List<TaggedWord>> words = new HashSet<>(l._3());
+                            List<TaggedWord> w2 = new LinkedList<>();
+                            value._1().forEach(x -> w2.addAll(x.words));
+                            words.add(w2);
+                            new_acc.add(new Tuple3<>(l._1(), l._2() + value._2(), words));
+                            foundOne = true;
+                            break;
+                        }
+                    }
+                    if (!foundOne){
+                        Set<List<TaggedWord>> words = new HashSet<>();
+                        List<TaggedWord> w2 = new LinkedList<>();
+                        value._1().forEach(x -> w2.addAll(x.words));
+                        words.add(w2);
+                        new_acc.add(new Tuple3<>(value._1(), value._2(), words));
+                    }
+                    return new_acc;
+
+                },
+                (List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> acc1, List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> acc2) -> {
+                    List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> dotProduct = new LinkedList<>();
+                    List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> result = new LinkedList<>();
+                    dotProduct.addAll(acc1);
+                    dotProduct.addAll(acc2);
+                    for (int i = 0; i< dotProduct.size(); i++){
                         Boolean foundOne = false;
-                        List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> new_acc = new LinkedList<>(acc);
-                        for (int i = 0; i<acc.size(); i++){
-                            Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>> l = acc.get(i);
-                            if(compare(value._1(), l._1())){
-                                new_acc.remove(i);
-                                Set<List<TaggedWord>> words = new HashSet<>(l._3());
-                                List<TaggedWord> w2 = new LinkedList<>();
-                                value._1().forEach(x -> w2.addAll(x.words));
-                                words.add(w2);
-                                new_acc.add(new Tuple3<>(l._1(), l._2() + value._2(), words));
+                        Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>> l1 = dotProduct.get(i);
+                        for (int j = i+1; j< dotProduct.size(); j++){
+                            Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>> l2 = dotProduct.get(j);
+                            if(compare(l1._1(), l2._1())){
+                                Set<List<TaggedWord>> words =  new HashSet<>(l1._3());
+                                words.addAll(l2._3());
+                                result.add(new Tuple3<>(l1._1(), l1._2()+l2._2(), words));
                                 foundOne = true;
                                 break;
                             }
                         }
                         if (!foundOne){
-                            Set<List<TaggedWord>> words = new HashSet<>();
-                            List<TaggedWord> w2 = new LinkedList<>();
-                            value._1().forEach(x -> w2.addAll(x.words));
-                            words.add(w2);
-                            new_acc.add(new Tuple3<>(value._1(), value._2(), words));
+                            result.add(new Tuple3<>(l1._1(), l1._2(), l1._3()));
                         }
-                        return new_acc;
-
-                    },
-                    (List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> acc1, List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> acc2) -> {
-                        List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> dotProduct = new LinkedList<>();
-                        List<Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>>> result = new LinkedList<>();
-                        dotProduct.addAll(acc1);
-                        dotProduct.addAll(acc2);
-                        for (int i = 0; i< dotProduct.size(); i++){
-                            Boolean foundOne = false;
-                            Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>> l1 = dotProduct.get(i);
-                            for (int j = i+1; j< dotProduct.size(); j++){
-                                Tuple3<List<VectorWithWords>, Integer, Set<List<TaggedWord>>> l2 = dotProduct.get(j);
-                                if(compare(l1._1(), l2._1())){
-                                    Set<List<TaggedWord>> words =  new HashSet<>(l1._3());
-                                    words.addAll(l2._3());
-                                    result.add(new Tuple3<>(l1._1(), l1._2()+l2._2(), words));
-                                    foundOne = true;
-                                    break;
-                                }
-                            }
-                            if (!foundOne){
-                                result.add(new Tuple3<>(l1._1(), l1._2(), l1._3()));
-                            }
-                        }
-                        return result;
                     }
-            );
+                    return result;
+                }
+        );
 
 
-            clusters.sort((a, b) -> b._2() - a._2());
-            clusters.stream().limit(25).forEach((t) -> {
-                List<TaggedWord> representation = t._1().stream().map((s) -> s.words.get(0)).collect(Collectors.toList());
-                System.out.println(representation.toString() + ": " + t._2().toString() + " | " + t._3().toString());
-                System.out.println("Feature: " + template.getFeature(representation));
+        clusters.sort((a, b) -> b._2() - a._2());
+        clusters.stream().limit(25).forEach((t) -> {
+            List<TaggedWord> representation = t._1().stream().map((s) -> s.words.get(0)).collect(Collectors.toList());
+            System.out.println(representation.toString() + ": " + t._2().toString() + " | " + t._3().toString());
+            System.out.println("Feature: " + template.getFeature(representation));
+        });
+        System.out.println(clusters.size());
+
+        Set<String> features1 = new HashSet<>();
+
+        Map<List<TaggedWord>, String> featureMap= new HashMap<>();
+        clusters.forEach((cluster) -> {
+                    List<TaggedWord> representation = cluster._1().stream().map((s) -> s.words.get(0)).collect(Collectors.toList());
+                    String feature = template.getFeature(representation);
+                    features1.add(feature);
+                    cluster._3().forEach(listOfTaggedWords ->{
+                        featureMap.put(listOfTaggedWords, feature);
+                    });
+
+                }
+        );
+        List<String> features = new ArrayList<>(features1);
+        Broadcast<List<String>> featureBroadcast = context.broadcast(features);
+        Broadcast<Map<List<TaggedWord>, String>> featureMapBroadcast = context.broadcast(featureMap);
+
+        JavaRDD points = tagRDD.map(rating -> {
+            List<String> fbc = featureBroadcast.getValue();
+            Map<List<TaggedWord>, String> fmp = featureMapBroadcast.getValue();
+            double[] v = new double[fbc.size()];
+            List<Tuple2<List<TaggedWord>, Integer>> output =  BigramThesis.findKGramsEx(3, rating._1, template);
+            output.forEach((Tuple2<List<TaggedWord>, Integer> feature) ->{
+                v[fbc.indexOf(fmp.get(feature._1()))] = feature._2(); //TODO: Index of is ugly and costs time
             });
-            System.out.println(clusters.size());
+            return new LabeledPoint((double) (rating._2), Vectors.dense(v));
+        });
 
-            Set<String> features1 = new HashSet<>();
-
-            Map<List<TaggedWord>, String> featureMap= new HashMap<>();
-            clusters.forEach((cluster) -> {
-                        List<TaggedWord> representation = cluster._1().stream().map((s) -> s.words.get(0)).collect(Collectors.toList());
-                        String feature = template.getFeature(representation);
-                        features1.add(feature);
-                        cluster._3().forEach(listOfTaggedWords ->{
-                            featureMap.put(listOfTaggedWords, feature);
-                        });
-
-                    }
-            );
-            List<String> features = new ArrayList<>(features1);
-            Broadcast<List<String>> featureBroadcast = context.broadcast(features);
-            Broadcast<Map<List<TaggedWord>, String>> featureMapBroadcast = context.broadcast(featureMap);
-
-            JavaRDD points = tagRDD.map(rating -> {
-                List<String> fbc = featureBroadcast.getValue();
-                Map<List<TaggedWord>, String> fmp = featureMapBroadcast.getValue();
-                double[] v = new double[fbc.size()];
-                List<Tuple2<List<TaggedWord>, Integer>> output =  BigramThesis.findKGramsEx(3, rating._1, template);
-                output.forEach((Tuple2<List<TaggedWord>, Integer> feature) ->{
-                    v[fbc.indexOf(fmp.get(feature._1()))] = feature._2(); //TODO: Index of is ugly and costs time
-                });
-                return new LabeledPoint((double) (rating._2), Vectors.dense(v));
-            });
-
-            DataFrame training = sqlContext.createDataFrame(points, LabeledPoint.class);
+        DataFrame training = sqlContext.createDataFrame(points, LabeledPoint.class);
 
 
-            org.apache.spark.ml.regression.LinearRegression lr = new org.apache.spark.ml.regression.LinearRegression();
+        org.apache.spark.ml.regression.LinearRegression lr = new org.apache.spark.ml.regression.LinearRegression();
 
-            lr.setMaxIter(10)
-                    .setRegParam(0.01);
+        lr.setMaxIter(10)
+                .setRegParam(0.01);
 
-            LinearRegressionModel model1 = lr.train(training);
+        LinearRegressionModel model1 = lr.train(training);
 
-            System.out.println("Model 1 was fit using parameters: " + model1.coefficients());
+        System.out.println("Model 1 was fit using parameters: " + model1.coefficients());
 
-            Map<String, Double> map = new HashMap<>();
-            double[] coeffs = model1.coefficients().toArray();
-            for (int i = 0; i < coeffs.length; i++) {
-                int index = i;
-                featureMap.entrySet().stream().filter((Map.Entry<List<TaggedWord>, String> entry) -> (features.indexOf(entry.getValue()) == index))
-                        .forEach((Map.Entry<List<TaggedWord>, String> entry2) -> {
-                            map.put(entry2.getValue(), coeffs[index]);
-                        });
+        Map<String, Double> map = new HashMap<>();
+        double[] coeffs = model1.coefficients().toArray();
+        for (int i = 0; i < coeffs.length; i++) {
+            int index = i;
+            featureMap.entrySet().stream().filter((Map.Entry<List<TaggedWord>, String> entry) -> (features.indexOf(entry.getValue()) == index))
+                    .forEach((Map.Entry<List<TaggedWord>, String> entry2) -> {
+                        map.put(entry2.getValue(), coeffs[index]);
+                    });
 
-            }
+        }
 
-            Iterator<Map.Entry<String, Double>> i = Utility.valueIterator(map);
-            System.out.println("Positive Words");
-            int j = 0;
-            while (j < 50) {
-                Map.Entry<String, Double> entry = i.next();
-                System.out.println(entry);
-                j++;
-            }
-            System.out.println("");
-            System.out.println("Negative Words");
-            i = Utility.valueIteratorReverse(map);
-            j = 0;
-            while (j < 50) {
-                Map.Entry<String, Double> entry = i.next();
-                System.out.println(entry);
-                j++;
-
-            }
-
+        Iterator<Map.Entry<String, Double>> i = Utility.valueIterator(map);
+        System.out.println("Positive Words");
+        int j = 0;
+        while (j < 50) {
+            Map.Entry<String, Double> entry = i.next();
+            System.out.println(entry);
+            j++;
+        }
+        System.out.println("");
+        System.out.println("Negative Words");
+        i = Utility.valueIteratorReverse(map);
+        j = 0;
+        while (j < 50) {
+            Map.Entry<String, Double> entry = i.next();
+            System.out.println(entry);
+            j++;
 
         }
 
