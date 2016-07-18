63e6ce50b1e6ffe53451741d1a20d153d04294d6
AxelKroschk <axel.kroschk@posteo.de>
2016-07-18 16:41:14 +0200

    added alternative in transposing matrix.

--- src/main/java/de/hpi/mmds/DIMSUM.java
+++ src/main/java/de/hpi/mmds/DIMSUM.java
@@ -10,6 +10,8 @@ import de.hpi.mmds.transpose;
 import scala.Tuple2;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 
@@ -25,13 +27,11 @@ public class DIMSUM{
 
         //repartitionedVectorRDD.cache();
         JavaPairRDD<Vector, Long> indexedVectors = repartitionedVectorRDD2.mapToPair((Tuple2<Tuple2<Main.Match, Integer>, Long> tuple) -> (new Tuple2<Vector, Long>(tuple._1()._1().getVectors().get(0).vector, tuple._2())));
-        JavaRDD<IndexedRow> rows = indexedVectors.map(tuple -> new IndexedRow(tuple._2(), tuple._1()));
 
-        IndexedRowMatrix mat = new IndexedRowMatrix(rows.rdd());
 
         System.out.println("Transposing Matrix");
-
-        RowMatrix mat2 = transpose.transposeRowMatrix(mat);
+        RowMatrix mat2 = computeRowmatrixViaCoordMat(indexedVectors);
+        //RowMatrix mat2 = computeRowmatrixViaTranspose(indexedVectors);
 
         System.out.println("computing similarities");
 
@@ -55,5 +55,28 @@ public class DIMSUM{
 
         return mergedVectorRDD;
 
+    }
+
+    private static RowMatrix computeRowmatrixViaTranspose(JavaPairRDD<Vector, Long> indexedVectors){
+        JavaRDD<IndexedRow> rows = indexedVectors.map(tuple -> new IndexedRow(tuple._2(), tuple._1()));
+
+        IndexedRowMatrix mat = new IndexedRowMatrix(rows.rdd());
+        return transpose.transposeRowMatrix(mat);
+    }
+
+    private static RowMatrix computeRowmatrixViaCoordMat(JavaPairRDD<Vector, Long> indexedVectors){
+
+        JavaRDD<MatrixEntry> asdf = indexedVectors.flatMap(vectorLongTuple2 -> {
+            double[] vector = vectorLongTuple2._1().toArray();
+            List<MatrixEntry> list = new LinkedList<>();
+            for (long i=0; i< vector.length; i++){
+                list.add(new MatrixEntry(i, vectorLongTuple2._2().longValue(), vector[(int) i]));
+            }
+            return list;
+        });
+
+        CoordinateMatrix cmat = new CoordinateMatrix(asdf.rdd());
+
+        return cmat.toRowMatrix();
     }
 }
